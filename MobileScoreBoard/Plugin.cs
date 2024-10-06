fe6a71f129c97a5bc7564ecf8f4b778433263141
shivriochilly <124175615+charabii@users.noreply.github.com>
2024-10-06 11:37:44 -0600

    Update Plugin.cs

--- MobileScoreBoard/Plugin.cs
+++ MobileScoreBoard/Plugin.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using BepInEx.Configuration;
 using System.Collections;
 using UnityEngine;
@@ -16,8 +16,9 @@ namespace MobileScoreBoard
         private bool pressingb;
         private float holdTimeCounter;
         private bool cooldown;
-        private bool cooldownnew;
+        private bool menuOpen;
         private ConfigEntry<int> holdTime;
+        private ConfigEntry<bool> shouldMove;
         private GorillaMetaReport gmet;
         private GorillaReportButton gmetb;
 
@@ -29,9 +30,10 @@ namespace MobileScoreBoard
         void OnGameInitialized()
         {
             holdTime = Config.Bind("Settings", "Hold Time", 4);
+            shouldMove = Config.Bind("Settings", "Should Move", true);
             gmet = FindObjectOfType<GorillaMetaReport>();
             gmetb = FindObjectOfType<GorillaReportButton>();
-            cooldownnew = false;
+            menuOpen = false;
         }
 
         void Update()
@@ -39,29 +41,35 @@ namespace MobileScoreBoard
             pressingb = ControllerInputPoller.instance.rightControllerSecondaryButton;
             pressing = ControllerInputPoller.instance.leftControllerSecondaryButton;
 
-            if (pressing)
+            if (pressing && !cooldown && !menuOpen)
             {
-                if (cooldownnew == false)
+                holdTimeCounter += Time.deltaTime;
+
+                if (holdTimeCounter >= holdTime.Value)
                 {
                     gmet.StartOverlay();
-                    cooldownnew = true;
+                    menuOpen = true;
+                    cooldown = true;
+                    holdTimeCounter = 0;
+                    StartCoroutine(Cooldown(1f));
                 }
             }
             else
             {
-                cooldownnew = false;
+                holdTimeCounter = 0;
             }
             gmet.isMoving = false;
 
             if (pressingb)
             {
                 gmetb.selected = true;
+                menuOpen = false;
             }
             else
             {
                 gmetb.selected = false;
             }
-            gmet.isMoving = true;
+            gmet.isMoving = shouldMove.Value;
         }
 
         IEnumerator Cooldown(float sec)
